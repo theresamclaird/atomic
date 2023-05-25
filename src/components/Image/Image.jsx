a5275b4cc472caa5862bda882da1c300fbf202c7
theresamclaird <theresa@mclairdfamily.com>
2023-05-25 09:13:18 -0700

    Allows srcSet on Image

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '../Box';
 
-function Image({ alt, src, ...props }) {
-  return <Box as="img" alt={alt} src={src} {...props} />;
+function Image({
+  alt, src, srcSets, ...props
+}) {
+  return (
+    <Box as="picture">
+      {srcSets.map((srcSet) => <Box as="source" srcSet={srcSet} />)}
+      <Box as="img" alt={alt} src={src} {...props} />
+    </Box>
+  );
 }
 
-Image.defaultProps = {};
+Image.defaultProps = {
+  srcSets: [],
+};
 
 Image.propTypes = {
   alt: PropTypes.string.isRequired,
   src: PropTypes.string.isRequired,
+  srcSets: PropTypes.arrayOf(PropTypes.string),
 };
 
 export { Image, Image as default };
--- src/components/Image/Image.stories.jsx
+++ src/components/Image/Image.stories.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { Image } from './Image';
-import imageFile from './IMG_0791.png';
 
 export default {
   title: 'Atomic/Image',
@@ -17,8 +16,9 @@ function Template(args) {
 
 export const Example = Template.bind({});
 Example.args = {
-  alt: 'Theresa and Dana',
-  src: imageFile,
+  alt: 'Theresa and Dana McLaird',
+  src: 'https://mclairdfamily-images.s3.us-west-2.amazonaws.com/mclairds.webp',
+  srcSets: ['https://mclairdfamily-images.s3.us-west-2.amazonaws.com/mclairds-300.webp 300w, https://mclairdfamily-images.s3.us-west-2.amazonaws.com/mclairds-600.webp 600w, https://mclairdfamily-images.s3.us-west-2.amazonaws.com/mclairds-900.webp 900w, https://mclairdfamily-images.s3.us-west-2.amazonaws.com/mclairds-1000.webp 1000w'],
   sx: {
     maxWidth: '50%',
     minWidth: '200px',
