fcb8a745ce2e109ada3ca8364a6aea91ae6326a8
theresamclaird <theresa@mclairdfamily.com>
2023-05-26 19:27:58 -0700

    themefies storybook & upgrades to latest 7.0.18

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,11 @@
+// .storybook/YourTheme.js
+
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Atomic Component Library',
+  brandUrl: 'https://main.d3g1ozoijvfcmm.amplifyapp.com/',
+  brandImage: 'https://mclairdfamily-images.s3.us-west-2.amazonaws.com/atomic-200-inverse.png',
+  brandTarget: '_self',
+});
