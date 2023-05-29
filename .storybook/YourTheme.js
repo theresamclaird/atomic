bb73a5ce2037d1cd4ff92b85143e0dd3356f8f70
theresamclaird <theresa@mclairdfamily.com>
2023-05-29 15:20:16 -0700

    changed the log again

--- .storybook/YourTheme.js
+++ .storybook/YourTheme.js
@@ -6,6 +6,6 @@ export default create({
   base: 'light',
   brandTitle: 'Atomic Component Library',
   brandUrl: 'https://main.d3g1ozoijvfcmm.amplifyapp.com/',
-  brandImage: 'https://mclairdfamily-images.s3.us-west-2.amazonaws.com/atomic-logo-825x200.png',
+  brandImage: 'https://mclairdfamily-images.s3.us-west-2.amazonaws.com/ACL-logo-full.png',
   brandTarget: '_self',
 });
