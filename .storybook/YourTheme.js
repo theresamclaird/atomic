875aadbd0073e0c32df1050480fb521b5ad0ba05
theresamclaird <theresa@mclairdfamily.com>
2023-05-29 12:33:52 -0700

    changed the logo

--- .storybook/YourTheme.js
+++ .storybook/YourTheme.js
@@ -3,9 +3,9 @@
 import { create } from '@storybook/theming/create';
 
 export default create({
-  base: 'dark',
+  base: 'light',
   brandTitle: 'Atomic Component Library',
   brandUrl: 'https://main.d3g1ozoijvfcmm.amplifyapp.com/',
-  brandImage: 'https://mclairdfamily-images.s3.us-west-2.amazonaws.com/atomic-300-inverse.png',
+  brandImage: 'https://mclairdfamily-images.s3.us-west-2.amazonaws.com/atomic-logo-825x200.png',
   brandTarget: '_self',
 });
