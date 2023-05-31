7f94e47ecc6475ade22d68316ae719f8cd8d53b0
theresamclaird <theresa@mclairdfamily.com>
2023-05-30 20:57:32 -0700

    Fixes Box story

--- src/components/Box/Box.stories.jsx
+++ src/components/Box/Box.stories.jsx
@@ -10,18 +10,19 @@ export default {
   },
 };
 
-function BoxTemplate(args) {
-  return <Box {...args} />;
+function Template(args) {
+  return (
+    <Box {...args}>
+      <Text>This text is inside a Box.</Text>
+    </Box>
+  );
 }
 
-export const Default = BoxTemplate.bind({});
-Default.args = {
-  children: <Text>This is a default Box.</Text>,
-};
+export const Default = Template.bind({});
+Default.args = {};
 
-export const StyledBox = BoxTemplate.bind({});
+export const StyledBox = Template.bind({});
 StyledBox.args = {
-  children: <Text>This Box is styled using the sx prop.</Text>,
   sx: {
     backgroundColor: '#609',
     border: 'solid 3px #000',
@@ -32,11 +33,16 @@ StyledBox.args = {
   },
 };
 
-export const UnorderedList = BoxTemplate.bind({});
+function ListTemplate(args) {
+  return (
+    <Box {...args}>
+      <Text as="li">First Bullet</Text>
+      <Text as="li">Second Bullet</Text>
+    </Box>
+  );
+}
+
+export const UnorderedList = ListTemplate.bind({});
 UnorderedList.args = {
-  children: [
-    <Text as="li">First Bullet</Text>,
-    <Text as="li">Second Bullet</Text>,
-  ],
   as: 'ul',
 };
