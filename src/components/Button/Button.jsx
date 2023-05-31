c7b8a80617a5ed7fba8c683520ae46d4a1e20ab9
theresamclaird <theresa@mclairdfamily.com>
2023-05-30 20:45:17 -0700

    adds onClick to Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '../Box';
 
-function Button({ label, sx, ...props }) {
-  return (<Box as="button" sx={{ ...sx }} {...props}>{label}</Box>);
+function Button({
+  label, onClick, sx, ...props
+}) {
+  return (<Box as="button" sx={{ ...sx }} onClick={onClick} {...props}>{label}</Box>);
 }
 
 Button.defaultProps = {
+  onClick: () => null,
   sx: {},
 };
 
 Button.propTypes = {
   label: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
   sx: PropTypes.objectOf(PropTypes.any),
 };
 
