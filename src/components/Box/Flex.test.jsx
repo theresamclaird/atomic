dd6af0f0fb0a1accf8c40c328e7d566fa352a742
theresamclaird <theresa@mclairdfamily.com>
2023-06-15 13:51:02 -0700

    Adds jest ignore for plop test template and test for Flex

---
+++ src/components/Box/Flex.test.jsx
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Flex } from './Flex';
+
+describe('Box.jsx', () => {
+  test('Renders child content', () => {
+    render(<Flex>TEST</Flex>);
+    expect(screen.getByText('TEST')).toBeInTheDocument();
+  });
+});
