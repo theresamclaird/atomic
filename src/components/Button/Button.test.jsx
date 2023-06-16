6adcb5ea908be26f53f1649756a0f5655ea65782
theresa@mclairdfamily.com <theresa@mclairdfamily.com>
2023-06-15 20:46:27 -0700

    adds onClick test to Button

--- src/components/Button/Button.test.jsx
+++ src/components/Button/Button.test.jsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Button } from './Button';
 
@@ -10,5 +10,12 @@ describe('Button', () => {
   test('Renders child content', () => {
     render(<Button label="TEST" />);
     expect(screen.getByText('TEST')).toBeInTheDocument();
+  });
+
+  test('executes onClick when clicked', () => {
+    const stub = jest.fn();
+    render(<Button label="TEST" onClick={stub} />);
+    fireEvent.click(screen.getByText('TEST'));
+    expect(stub).toBeCalled();
   });
 });
