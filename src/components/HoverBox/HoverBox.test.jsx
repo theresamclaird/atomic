5edbf3b7db55fcf422551a5efe739e3351a5e1b7
theresamclaird <theresa@mclairdfamily.com>
2023-06-16 12:53:40 -0700

    adds tests for HoverBox

---
+++ src/components/HoverBox/HoverBox.test.jsx
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import {
+  render, screen, fireEvent, waitFor,
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { HoverBox } from './HoverBox';
+import { Text } from '../Text';
+
+describe('HoverBox', () => {
+  test('renders child content', async () => {
+    render(
+      <HoverBox>
+        {(mouseEntered) => <Text>{mouseEntered ? 'YES' : 'NO'}</Text>}
+      </HoverBox>,
+    );
+    expect(screen.getByText('NO')).toBeVisible();
+
+    fireEvent.mouseEnter(screen.getByText('NO'));
+    await waitFor(() => screen.getByText('YES'));
+    expect(screen.getByText('YES')).toBeVisible();
+  });
+});
