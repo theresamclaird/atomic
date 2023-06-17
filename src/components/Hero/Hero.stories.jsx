3f770c72da30d618d1bafa9a12a09e026ab3c98d
theresamclaird <theresa@mclairdfamily.com>
2023-06-16 19:45:00 -0700

    adds test for Hero

--- src/components/Hero/Hero.stories.jsx
+++ src/components/Hero/Hero.stories.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Hero } from './Hero';
-import { Button, Flex, Text } from '..';
+import { Button, Text } from '..';
 
 export default {
   title: 'Molecules/Hero',
@@ -12,19 +12,17 @@ export default {
 
 function HeroTemplate(args) {
   return (
-    <Flex justify="center" sx={{ alignItems: 'flex-start', mb: 1, backgroundColor: 'background' }}>
-      <Hero {...args}>
-        <Text as="h2">Llama Cria</Text>
-        <Text sx={{ mt: 'small' }}>One of the most interesting facts about llama cria is their ability to recognize and remember their mothers even after a long separation. Llama cria have a remarkable memory and can identify their mothers by their scent and unique vocalizations, even if they have been separated for an extended period. This strong bond between a llama cria and its mother highlights the depth of their social and familial connections. It is quite remarkable that despite growing up and potentially forming relationships with other llamas, the cria can still distinguish and remember their own mothers, showcasing their exceptional cognitive abilities and memory skills.</Text>
-        <Button
-          onClick={() => {
-            window.open('https://en.wikipedia.org/wiki/Cria', '_blank');
-          }}
-          label="Wikipedia"
-          sx={{ mt: 1 }}
-        />
-      </Hero>
-    </Flex>
+    <Hero {...args}>
+      <Text as="h2">Llama Cria</Text>
+      <Text sx={{ mt: 'small' }}>One of the most interesting facts about llama cria is their ability to recognize and remember their mothers even after a long separation. Llama cria have a remarkable memory and can identify their mothers by their scent and unique vocalizations, even if they have been separated for an extended period. This strong bond between a llama cria and its mother highlights the depth of their social and familial connections. It is quite remarkable that despite growing up and potentially forming relationships with other llamas, the cria can still distinguish and remember their own mothers, showcasing their exceptional cognitive abilities and memory skills.</Text>
+      <Button
+        onClick={() => {
+          window.open('https://en.wikipedia.org/wiki/Cria', '_blank');
+        }}
+        label="Wikipedia"
+        sx={{ mt: 1 }}
+      />
+    </Hero>
   );
 }
 
@@ -37,5 +35,4 @@ Default.args = {
     { srcset: 'https://mclairdfamily-images.s3.us-west-2.amazonaws.com/demo-300x185.jpg 300w, https://mclairdfamily-images.s3.us-west-2.amazonaws.com/demo-600x371.jpg 600w, https://mclairdfamily-images.s3.us-west-2.amazonaws.com/demo-900x556.jpg 900w, https://mclairdfamily-images.s3.us-west-2.amazonaws.com/demo-1200x742.jpg 1200w, https://mclairdfamily-images.s3.us-west-2.amazonaws.com/demo-2400x1483.jpg 2400w, https://mclairdfamily-images.s3.us-west-2.amazonaws.com/demo-3600x2225.jpg 3600w' },
   ],
   aspectRatio: 1.618,
-  sx: { width: ['100%', '80%', '50%'], my: [0, '1rem'] },
 };
---
+++ src/components/Hero/Hero.test.jsx
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Hero } from './Hero';
+
+describe('Hero', () => {
+  test('renders child content', () => {
+    render(
+      <Hero
+        alt="TESTALT"
+        src="https://mclairdfamily-images.s3.us-west-2.amazonaws.com/demo-300x185.webp"
+        aspectRatio={1.618}
+      >
+        TEST
+      </Hero>,
+    );
+
+    expect(screen.getByAltText('TESTALT')).toBeVisible();
+    expect(screen.getByText('TEST')).toBeVisible();
+  });
+});
