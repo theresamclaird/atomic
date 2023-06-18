a843e89284c9a5ab6111023b44e79fadd1a6f450
theresamclaird <theresa@mclairdfamily.com>
2023-06-18 08:40:28 -0700

    removes actions storybook panel

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,17 @@
 module.exports = {
-  "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm", "@storybook/addon-a11y"],
-  "framework": {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    { name: "@storybook/addon-essentials", options: { actions: false } },
+    // "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-a11y"],
+  framework: {
     name: "@storybook/react-webpack5",
     options: {}
   },
   docs: {
     autodocs: true
   },
-  staticDirs: ['./public']
+  staticDirs: ['./public'],
 };
