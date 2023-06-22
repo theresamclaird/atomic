d048d0a52bebfc81e0b9fd1a10db9825f4ee224b
Nathan F Syfrig <nathan@artisticnetwork.com>
2023-06-22 14:08:15 -0700

    Making the Metronome specifications as much of an array as current knowledge of React allows (questions in-line in comments)

--- src/components/Metronome/Metronome.jsx
+++ src/components/Metronome/Metronome.jsx
@@ -5,31 +5,49 @@ import { Text } from '../Text';
 import { useMetronome } from './useMetronome';
 import { Flasher } from './Flasher';
 
-function Metronome({ startMs, beatsPerMinuteX, bpmY, bpmZ, sx, sy, sz, ...props }) {
-  const [whole] = useMetronome(beatsPerMinuteX / 4, startMs);
-  const [half] = useMetronome(beatsPerMinuteX / 2, startMs);
-  const [quarter] = useMetronome(beatsPerMinuteX, startMs);
-  const [eighth] = useMetronome(beatsPerMinuteX * 2, startMs);
-  const [sixteenth] = useMetronome(beatsPerMinuteX * 2, startMs);
-  const [thirtysecond] = useMetronome(beatsPerMinuteX * 2, startMs);
+function Metronome({ startMs, metronomeNumber, beatsPerMinutes, sx, ...props }) {
+  const [whole] = useMetronome(beatsPerMinutes[metronomeNumber] / 4, startMs);
+  const [half] = useMetronome(beatsPerMinutes[metronomeNumber] / 2, startMs);
+  const [quarter] = useMetronome(beatsPerMinutes[metronomeNumber], startMs);
+  const [eighth] = useMetronome(beatsPerMinutes[metronomeNumber] * 2, startMs);
+  const [sixteenth] = useMetronome(beatsPerMinutes[metronomeNumber] * 4, startMs);
+  const [thirtysecond] = useMetronome(beatsPerMinutes[metronomeNumber] * 8, startMs);
 
-  const [wholey] = useMetronome(bpmY / 4, startMs);
-  const [halfy] = useMetronome(bpmY / 2, startMs);
-  const [quartery] = useMetronome(bpmY, startMs);
-  const [eighthy] = useMetronome(bpmY * 2, startMs);
-  const [sixteenthy] = useMetronome(bpmY * 2, startMs);
-  const [thirtysecondy] = useMetronome(bpmY * 2, startMs);
+  const [whole1] = useMetronome(beatsPerMinutes[metronomeNumber + 1] / 4, startMs);
+  const [half1] = useMetronome(beatsPerMinutes[metronomeNumber + 1] / 2, startMs);
+  const [quarter1] = useMetronome(beatsPerMinutes[metronomeNumber + 1], startMs);
+  const [eighth1] = useMetronome(beatsPerMinutes[metronomeNumber + 1] * 2, startMs);
+  const [sixteenth1] = useMetronome(beatsPerMinutes[metronomeNumber + 1] * 4, startMs);
+  const [thirtysecond1] = useMetronome(beatsPerMinutes[metronomeNumber + 1] * 8, startMs);
 
-  const [wholez] = useMetronome(bpmZ / 4, startMs);
-  const [halfz] = useMetronome(bpmZ / 2, startMs);
-  const [quarterz] = useMetronome(bpmZ, startMs);
-  const [eighthz] = useMetronome(bpmZ * 2, startMs);
-  const [sixteenthz] = useMetronome(bpmZ * 2, startMs);
-  const [thirtysecondz] = useMetronome(bpmZ * 2, startMs);
+  const [whole2] = useMetronome(beatsPerMinutes[metronomeNumber + 2] / 4, startMs);
+  const [half2] = useMetronome(beatsPerMinutes[metronomeNumber + 2] / 2, startMs);
+  const [quarter2] = useMetronome(beatsPerMinutes[metronomeNumber + 2], startMs);
+  const [eighth2] = useMetronome(beatsPerMinutes[metronomeNumber + 2] * 2, startMs);
+  const [sixteenth2] = useMetronome(beatsPerMinutes[metronomeNumber + 2] * 4, startMs);
+  const [thirtysecond2] = useMetronome(beatsPerMinutes[metronomeNumber + 2] * 8, startMs);
+
+  /*
+   * I want to be able to get the constants defined above as elements of the array as
+   *  defined in the property types
+   */
+  // function GetBpmsFromMetronomeIndex(metronomeIndex) {
+  //   return {
+  //     metronomeWhole: useMetronome(beatsPerMinutes[metronomeIndex] / 4, startMs),
+  //     metronomeHalf: useMetronome(beatsPerMinutes[metronomeIndex] / 2, startMs),
+  //     metronomeQuarter: useMetronome(beatsPerMinutes[metronomeIndex], startMs),
+  //     metronomeEighth: useMetronome(beatsPerMinutes[metronomeIndex] * 2, startMs),
+  //     metronomeSixteenth: useMetronome(beatsPerMinutes[metronomeIndex] * 4, startMs),
+  //     metronomeThirtysecond: useMetronome(beatsPerMinutes[metronomeIndex] * 8, startMs)
+  //   };
+  // }
 
   return (
     <Flex direction="column" justify="space-between" align="center">
-      <Text>{`BPM: ${beatsPerMinuteX}`}</Text>
+      {/* Want to be able to specify one Flex object as an index of array constants obtained
+       * from GetBpmsFromMetronomeIndex - and iterate over the array objects
+       */}
+      <Text>{`BPM: ${beatsPerMinutes[metronomeNumber]}`}</Text>
       <Flex sx={{ ...sx }} {...props}>
         <Flasher label="whole" beatCount={whole.beatCount} />
         <Flasher label="half" beatCount={half.beatCount} />
@@ -38,45 +56,43 @@ function Metronome({ startMs, beatsPerMinuteX, bpmY, bpmZ, sx, sy, sz, ...props 
         <Flasher label="sixteenth" beatCount={sixteenth.beatCount} />
         <Flasher label="thirtysecond" beatCount={thirtysecond.beatCount} />
       </Flex>
-      <Text>{`BPM: ${bpmY}`}</Text>
-      <Flex sy={{ ...sy }} {...props}>
-        <Flasher label="whole" beatCount={wholey.beatCount} />
-        <Flasher label="half" beatCount={halfy.beatCount} />
-        <Flasher label="quarter" beatCount={quartery.beatCount} />
-        <Flasher label="eighth" beatCount={eighthy.beatCount} />
-        <Flasher label="sixteenth" beatCount={sixteenthy.beatCount} />
-        <Flasher label="thirtysecond" beatCount={thirtysecondy.beatCount} />
+      {/* The rest of this specification should go away and be addressed as part of iterating
+       * through the array
+       */}
+      <Text>{`BPM: ${beatsPerMinutes[metronomeNumber + 1]}`}</Text>
+      <Flex sx={{ ...sx }} {...props}>
+        <Flasher label="whole" beatCount={whole1.beatCount} />
+        <Flasher label="half" beatCount={half1.beatCount} />
+        <Flasher label="quarter" beatCount={quarter1.beatCount} />
+        <Flasher label="eighth" beatCount={eighth1.beatCount} />
+        <Flasher label="sixteenth" beatCount={sixteenth1.beatCount} />
+        <Flasher label="thirtysecond" beatCount={thirtysecond1.beatCount} />
       </Flex>
-      <Text>{`BPM: ${bpmZ}`}</Text>
-      <Flex sy={{ ...sz }} {...props}>
-        <Flasher label="whole" beatCount={wholez.beatCount} />
-        <Flasher label="half" beatCount={halfz.beatCount} />
-        <Flasher label="quarter" beatCount={quarterz.beatCount} />
-        <Flasher label="eighth" beatCount={eighthz.beatCount} />
-        <Flasher label="sixteenth" beatCount={sixteenthz.beatCount} />
-        <Flasher label="thirtysecond" beatCount={thirtysecondz.beatCount} />
+      <Text>{`BPM: ${beatsPerMinutes[metronomeNumber + 2]}`}</Text>
+      <Flex sx={{ ...sx }} {...props}>
+        <Flasher label="whole" beatCount={whole2.beatCount} />
+        <Flasher label="half" beatCount={half2.beatCount} />
+        <Flasher label="quarter" beatCount={quarter2.beatCount} />
+        <Flasher label="eighth" beatCount={eighth2.beatCount} />
+        <Flasher label="sixteenth" beatCount={sixteenth2.beatCount} />
+        <Flasher label="thirtysecond" beatCount={thirtysecond2.beatCount} />
       </Flex>
     </Flex>
   );
 }
 
 Metronome.defaultProps = {
-  beatsPerMinuteX: 60,
-  bpmY: 120,
-  bpmZ: 240,
+  metronomeNumber: 0,
+  beatsPerMinutes: [60, 120, 240],
   sx: {},
-  sy: {},
-  sz: {},
 };
 
 Metronome.propTypes = {
   startMs: PropTypes.number.isRequired,
+  metronomeNumber: PropTypes.number,
   sx: PropTypes.objectOf(PropTypes.any),
-  sy: PropTypes.objectOf(PropTypes.any),
-  sz: PropTypes.objectOf(PropTypes.any),
-  beatsPerMinuteX: PropTypes.number,
-  bpmY: PropTypes.number,
-  bpmZ: PropTypes.number,
+  // The following should be specified as array of numbers, not any.  How do you do that?
+  beatsPerMinutes: PropTypes.array,
 };
 
 export { Metronome, Metronome as default };
